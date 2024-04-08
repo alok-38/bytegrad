2351f80e730049c7305b3306342da52d05785da0
Alokananda Y <plantsfortrees@gmail.com>
2024-04-08 08:56:33 +0530

    hook

---
+++ professional-react-and-next/usestate-hook/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './components/Header';
+import Counter from './components/Counter';
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <Header />
+    <Counter />
+    </>
+  )
+}
+
+export default App
---
+++ professional-react-and-next/usestate-hook/src/components/Counter.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+function Counter() {
+    const [count, setCount] = useState(0)
+    return (
+        <>
+            <div className='flex flex-col mt-10'>
+                <button className='border-solid border-2 border-orange-600 w-10 mx-auto rounded-3xl	 h-15 hover:bg-orange-400 hover:text-white font-thin	 text-5xl' onClick={() => setCount((count) => count + 1)}>
+                     +
+                </button>
+                        <span className='text-[100px] text-green-500'>{count}</span>
+                        <button className='border-solid border-2 border-red-600 w-10 mx-auto rounded-3xl h-15 hover:bg-red-400 hover:text-white font-thin	 text-5xl'  onClick={() => setCount((count) => count -1)}>
+                     -
+                </button>
+            </div>
+        </>
+    );
+}
+
+export default Counter;
