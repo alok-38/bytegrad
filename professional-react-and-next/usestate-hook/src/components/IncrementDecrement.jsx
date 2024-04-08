95fc3ccae09da6c2deb09db7f53b42bc821fdbfa
Alokananda Y <plantsfortrees@gmail.com>
2024-04-08 17:23:22 +0530

    hooks

--- professional-react-and-next/usestate-hook/src/components/IncrementDecrement.jsx
+++ professional-react-and-next/usestate-hook/src/components/IncrementDecrement.jsx
@@ -1,14 +1,25 @@
-function IncrementDecrement() {
+import { useState } from "react";
 
-  return (
-    <>
-        <div className="flex justify-center gap-5 mt-10">
-            <button className="border-2 border-red-500 text-5xl h-15 rounded-2xl w-20 hover:bg-red-600 hover:text-white">-</button>
-            <span className="text-[64px] text-blue-600">0</span>
-            <button className="border-2 border-green-500 text-5xl h-15 rounded-2xl w-20 hover:bg-green-600 hover:text-white">+</button>
-        </div>
-    </>
-  )
+function IncrementDecrement() {
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount(count + 1);
+    }
+
+    const decrement = () => {
+        setCount(count - 1);
+    }
+
+    return (
+        <>
+            <div className="flex justify-center gap-5 mt-10">
+                <button onClick={decrement} className="border-2 border-red-500 text-5xl h-15 rounded-2xl w-20 hover:bg-red-600 hover:text-white">-</button>
+                <span className="text-[64px] text-blue-600">{count}</span>
+                <button onClick={increment} className="border-2 border-green-500 text-5xl h-15 rounded-2xl w-20 hover:bg-green-600 hover:text-white">+</button>
+            </div>
+        </>
+    );
 }
 
-export default IncrementDecrement
+export default IncrementDecrement;
