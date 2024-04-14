9912c439fe1f3f1cfa6e7fd3900c1f76a17de722
Alokananda Y <plantsfortrees@gmail.com>
2024-04-14 10:22:32 +0530

    main

--- professional-react-and-next/my-project/src/App.jsx
+++ professional-react-and-next/my-project/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import Card from "./components/Card"
 
 function App() {
 
   return (
     <>
       <Header />
+      <Card />
       <Footer />
     </>
   )
