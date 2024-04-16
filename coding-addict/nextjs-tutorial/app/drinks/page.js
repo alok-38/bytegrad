8b6b137260735556bcac36c95f0b279313f704c0
Alokananda Y <plantsfortrees@gmail.com>
2024-04-16 16:54:35 +0530

    fetch

--- coding-addict/nextjs-tutorial/app/drinks/page.js
+++ coding-addict/nextjs-tutorial/app/drinks/page.js
@@ -1,13 +1,16 @@
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a';
 
 const fetchDrinks = async () => {
-  await new Promise((resolve) => {
-    setTimeout(resolve, 1000)
-  });
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch(url);
-  const data  = await response.json();
+  // throw error
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch drinks');
+  }
+  const data = await response.json();
   return data;
-}
+};
 
 const DrinksPage = async () => {
   const data = await fetchDrinks();
