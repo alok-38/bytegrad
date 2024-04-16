ca450f13dbaa21c9aa4e3ce325a0fac6b8ff5b60
Alokananda Y <plantsfortrees@gmail.com>
2024-04-16 13:24:46 +0530

    info

---
+++ coding-addict/nextjs-tutorial/app/about/info/page.js
@@ -0,0 +1,7 @@
+export default function AboutInfoPage() {
+    return (
+      <div>
+        <h1 className="text-7xl">About Info Page</h1>
+      </div>
+    )
+  }
---
+++ coding-addict/nextjs-tutorial/app/about/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function AboutPage() {
+    return (
+      <div>
+        <h1 className="text-7xl">About Page</h1>
+        <Link href='/' className="text-2xl">Home Page</Link>
+      </div>
+    )
+  }
