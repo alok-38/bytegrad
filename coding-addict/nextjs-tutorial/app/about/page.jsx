f85b892d55d92c338cd4be8d13ad716df0f15010
Alokananda Y <plantsfortrees@gmail.com>
2024-04-16 13:19:03 +0530

    link

--- coding-addict/nextjs-tutorial/app/about/page.jsx
+++
@@ -1,5 +0,0 @@
-export default function AboutPage() {
-    return (
-      <h1 className="text-7xl">About Page</h1>
-    )
-  }
--- coding-addict/nextjs-tutorial/app/page.js
+++ coding-addict/nextjs-tutorial/app/page.js
@@ -1,5 +1,10 @@
+import Link from "next/link"
+
 export default function HomePage() {
   return (
-    <h1 className="text-7xl">Home Page</h1>
+    <div>
+      <h1 className="text-7xl">Home Page</h1>
+      <Link href='/about' className="text-2xl">About Page</Link>
+    </div>
   )
 }
