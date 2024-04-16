a6e941ad16501e3891c4aa405661f9c0773f54e2
Alokananda Y <plantsfortrees@gmail.com>
2024-04-16 15:36:37 +0530

    layout

--- coding-addict/nextjs-tutorial/app/page.js
+++ coding-addict/nextjs-tutorial/app/page.js
@@ -1,9 +1,6 @@
-import Navbar from '../components/Navbar'
-
 export default function HomePage() {
   return (
     <div>
-      <Navbar />
       <h1 className="text-7xl">Home Page</h1>
     </div>
   )
