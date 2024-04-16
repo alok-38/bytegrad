b3c7dffa5aa43eb3f5c6641265508bf3586f7efa
Alokananda Y <plantsfortrees@gmail.com>
2024-04-16 15:54:09 +0530

    Counter Challenge

--- coding-addict/nextjs-tutorial/app/page.js
+++ coding-addict/nextjs-tutorial/app/page.js
@@ -1,7 +1,12 @@
+import Link from "next/link"
+
 export default function HomePage() {
   return (
     <div>
-      <h1 className="text-7xl">Home Page</h1>
+      <h1 className="text-7xl">Next.js Tutorial</h1>
+      <Link href='/client' className="btn btn-accent mt-5">
+        Get started
+      </Link>
     </div>
   )
 }
