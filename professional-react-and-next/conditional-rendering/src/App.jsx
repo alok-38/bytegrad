75d80012874bbad9bd3400424cdeee8cd7dc490c
Alokananda Y <plantsfortrees@gmail.com>
2024-04-16 12:34:29 +0530

    next

--- professional-react-and-next/conditional-rendering/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import Greeting from './components/Greeting';
-import './App.css';
-
-export default function App() {
-  return (
-    <Greeting  isLoggedIn={true} username="Alok"/>
-  );
-}
--- professional-react-and-next/conditional-rendering/src/components/Greeting.jsx
+++
@@ -1,9 +0,0 @@
-export default function Greeting(props) {
-
-    return (
-        props.isLoggedIn ? <h2>Welcome {props.username}</h2> :
-                            <h2>Please log in to continue</h2>
-        
-    );
-    
-}
