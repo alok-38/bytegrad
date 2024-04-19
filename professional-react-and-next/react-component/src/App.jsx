6a1c607060bf3881ee47cd432a5e113df0e9f3ce
Alokananda Y <plantsfortrees@gmail.com>
2024-04-19 08:19:26 +0530

    counter

--- professional-react-and-next/react-component/src/App.jsx
+++ professional-react-and-next/react-component/src/App.jsx
@@ -3,9 +3,9 @@ import Counter from "./components/Counter"
 function App() {
 
   return (
-    <>
-      <Counter />
-    </>
+    <div className="bg-lime-400 w-96 h-96 flex justify-center mx-auto my-[5%] items-center leading-10">
+        <Counter />
+    </div>
   )
 }
 
--- professional-react-and-next/react-component/src/components/Counter.jsx
+++ professional-react-and-next/react-component/src/components/Counter.jsx
@@ -1,12 +1,29 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 export default function Counter() {
     const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount(count + 1);
+    };
+
+    const decrement = () => {
+        if (count > 0) {
+            setCount(count - 1);
+        }
+    };
+
     return (
-        <div className="card">
-            <button onClick={() => setCount((count) => count + 1)}>
-                count is {count}
-            </button>
+        <div className='flex flex-col items-center'> {/* Container with flex column layout and center alignment */}
+            <div className='flex flex-row mb-10 gap-10 text-white'> {/* Button container with flex row layout */}
+                <button onClick={increment} className='border-solid border-[1px] border-green-600 hover:bg-green-600 w-32 rounded-full leading-10'> {/* Margin right to separate buttons */}
+                    Increment 
+                </button>
+                <button onClick={decrement} className='border-solid border-[1px] border-red-600 hover:bg-red-600 w-32 rounded-full leading-10'>
+                    Decrement  
+                </button>
+            </div>
+            <span className='font-semi text-zinc-700 text-[7em]'>{count}</span>
         </div>
     );
 }
