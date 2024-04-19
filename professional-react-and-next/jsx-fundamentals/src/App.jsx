cbbe60b43489ea02d5ce8e0061fb8f07ace84354
Alokananda Y <plantsfortrees@gmail.com>
2024-04-19 07:15:12 +0530

    counter

---
+++ professional-react-and-next/jsx-fundamentals/src/App.jsx
@@ -0,0 +1,9 @@
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <div className="m-h-screen flex justify-center items-center flex-col text-center">
+      <Header />
+    </div>
+  );
+}
