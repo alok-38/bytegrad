f0004d604d4f0dd4cdcc256332adb5d6878c70b7
Alokananda Y <plantsfortrees@gmail.com>
2024-04-21 08:05:36 +0530

    refactored

--- professional-javascript/fancy-counter/app.js
+++ professional-javascript/fancy-counter/app.js
@@ -7,24 +7,49 @@ const decreaseButton = document.querySelector(".counter__button--decrease");
 // Function to increase the count
 const increaseCount = () => {
   let currentCounterValue = parseInt(spanElement.textContent);
-  let newCounterValue = currentCounterValue + 1;
-  spanElement.textContent = newCounterValue;
-  // Disable the "+" button and update the h1 element if counter reaches 5
-  if (newCounterValue >= 5) {
-    increaseButton.disabled = true;
-    h1Element.textContent = "Please upgrade to pro";
+  // Check if the counter value is less than 5 before incrementing
+  if (currentCounterValue < 5) {
+    let newCounterValue = currentCounterValue + 1;
+    spanElement.textContent = newCounterValue;
+    // Disable the "+" button and update the h1 element if counter reaches 5
+    if (newCounterValue >= 5) {
+      h1Element.innerHTML = "Limit! Buy <b>Pro</b> for >5";
+      // Disable both increase and decrease buttons
+      increaseButton.disabled = true;
+      decreaseButton.disabled = true;
+      // Unfocus
+      increaseButton.blur();
+    }
   }
 };
 
 increaseButton.addEventListener("click", increaseCount);
 
+// Add event listener for keydown event
+document.addEventListener("keydown", (event) => {
+  // Check if the pressed key is the "ArrowUp" key or spacebar and if the counter is less than 5
+  if (
+    (event.key === "ArrowUp" || event.key === " ") &&
+    parseInt(spanElement.textContent) < 5
+  ) {
+    increaseCount();
+  }
+});
+
 // Function to decrease the count
 const decreaseCount = () => {
   let currentCounterValue = parseInt(spanElement.textContent);
   let newCounterValue = currentCounterValue - 1;
+  // If the new value is less than 0
+  if (newCounterValue < 0) {
+    // Force it to remain at 0
+    newCounterValue = 0;
+  }
+  // Update the displayed value in the HTML
   spanElement.textContent = newCounterValue;
-  // Enable the "+" button and update the h1 element if counter is less than 5
+  // Enable both buttons and update the title if counter drops below 5
   if (newCounterValue < 5) {
+    decreaseButton.disabled = false;
     increaseButton.disabled = false;
     h1Element.textContent = "Fancy Counter";
   }
@@ -36,7 +61,11 @@ decreaseButton.addEventListener("click", decreaseCount);
 const resetCount = () => {
   spanElement.textContent = "0"; // Reset the counter value to 0
   increaseButton.disabled = false; // Ensure the increase button is enabled
+  decreaseButton.disabled = false;
   h1Element.textContent = "Fancy Counter";
+
+  // Remove the keydown event listener
+  document.removeEventListener("keydown", increaseCount);
 };
 
 resetButton.addEventListener("click", resetCount);
