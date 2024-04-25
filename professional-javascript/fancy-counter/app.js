d88ae18d2a820597c9f4c6643d4addc772782bed
Alokananda Y <plantsfortrees@gmail.com>
2024-04-25 18:08:23 +0530

    JS

---
+++ professional-javascript/fancy-counter/app.js
@@ -0,0 +1,6 @@
+const h1Element = document.querySelector('.counter__title');
+const spanElement = document.querySelector('.counter__value');
+const resetButton = document.querySelector('.counter__reset-button');
+const decreaseButton = document.querySelector('.counter__button--decrease')
+const increaseButton = document.querySelector('.counter__button--increase');
+
