98fad1ce65b8ef50e140ecbe2a1827862b954726
Alokananda Y <plantsfortrees@gmail.com>
2024-04-26 10:40:06 +0530

    WORDANALYTICS

--- professional-javascript/word-analytics/app.js
+++ professional-javascript/word-analytics/app.js
@@ -5,16 +5,26 @@ const twitterStats = document.querySelector('.stat__number--twitter');
 const facebookStats = document.querySelector('.stat__number--facebook');
 
 let spaceBarCount = 0;
+let twitterCharacterLimit = parseInt(twitterStats.textContent);
+const facebookCharacterLimit = parseInt(facebookStats.textContent);
 
 const updateCounts = () => {
 	const text = textareaElement.value.trim();
 
 	// Update word count
-	const words = text.split(/\s+/).filter(word => word.length > 1);
+	const words = text.split(/\s+/).filter(word => word.length > 0);
 	wordsSpanElement.textContent = words.length || 0;
 
 	// Update character count
 	charsSpanElement.textContent = text.length + spaceBarCount;
+
+	// Update Twitter character count
+	const remainingTwitterCharacters = twitterCharacterLimit - text.length - spaceBarCount;
+	twitterStats.textContent = remainingTwitterCharacters >= 0 ? remainingTwitterCharacters : 0;
+
+	// Update Facebook
+	const remainingFacebookCharacters = facebookCharacterLimit - text.length - spaceBarCount;
+	facebookStats.textContent = remainingFacebookCharacters >= 0 ? remainingFacebookCharacters : 0;
 };
 
 // Add event listener for input events on textarea
@@ -24,12 +34,10 @@ textareaElement.addEventListener('input', updateCounts);
 textareaElement.addEventListener('keydown', (event) => {
 	if (event.key === ' ') {
 		spaceBarCount++;
-		updateCounts();
 	} else if (event.key === 'Backspace') {
 		if (spaceBarCount > 0) {
 			spaceBarCount--; // Decrement spaceBarCount by 1
 		}
-		updateCounts();
 	}
+	updateCounts();
 });
-
