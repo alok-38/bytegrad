1166a1f9bf1e32b0dc46f649d5884399b846a949
Alokananda Y <plantsfortrees@gmail.com>
2024-05-02 08:58:52 +0530

    arrays

---
+++ coding-addict/javascript-course/string-challenge/script.js
@@ -0,0 +1,6 @@
+const fullName = (firstName, lastName) => {
+  const concatenatedName = `${firstName} ${lastName}`;
+  return concatenatedName.toUpperCase();
+};
+
+console.log(fullName("Alokananda", "Y"));
