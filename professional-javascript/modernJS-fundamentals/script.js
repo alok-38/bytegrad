f751eb3a9f5c9a1e6524dcb9a70e122c8aab0e80
Alokananda Y <plantsfortrees@gmail.com>
2024-05-02 14:55:37 +0530

    modernJS-fundamentals

---
+++ professional-javascript/modernJS-fundamentals/script.js
@@ -0,0 +1,8 @@
+const description = "We need a new floor";
+const squareMeters = 100;
+const specialCoating = true;
+const floorOptions = ["carpet", "hardwood", "tiles"];
+const renovationJob = {
+	ownerName: 'John',
+	maximumPrice: 5000
+}
--- professional-javascript/remove-lastitem/app.js
+++
@@ -1,9 +0,0 @@
-let fruit = ['apple', 'orange', 'banana', 'tomato'];
-let popped = fruit.pop();
-
-console.log(popped); // "tomato"
-console.log(fruit); // ["apple", "orange", "banana"]
-
-let splicedFruit = fruit.splice(-1)
-console.log(splicedFruit);
-console.log(fruit);
