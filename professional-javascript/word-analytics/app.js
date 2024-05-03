e47e18ecf2e0d56ea86392738eda61291dcaa4c1
Alokananda Y <plantsfortrees@gmail.com>
2024-05-03 17:56:47 +0530

    WORDANALYTICS

--- professional-javascript/word-analytics/app.js
+++ professional-javascript/word-analytics/app.js
@@ -1,6 +1,8 @@
 const textAreaElement = document.querySelector(".textarea");
 const spanCharElement = document.querySelector(".stat__number--characters");
 const spanWordElement = document.querySelector(".stat__number--words");
+const spanTwitterElement = document.querySelector(".stat__number--twitter");
+const spanFacebookElement = document.querySelector(".stat__number--facebook");
 
 function updateCharCount() {
   const charCount = textAreaElement.value.length;
@@ -8,8 +10,14 @@ function updateCharCount() {
 }
 
 // Update character count on input
-textAreaElement.addEventListener("input", updateCharCount);
+textAreaElement.addEventListener("input", () => {
+  updateCharCount();
+  updateWordCount();
+  updateTwitterCharLimit(); // Update Twitter character limit on input
+  updateFacebookCharLimit();
+});
 
+// Word count function
 function updateWordCount() {
   const text = textAreaElement.value.trim(); // Remove leading and trailing spaces
   const words = text.split(/\s+/).filter((word) => word !== ""); // Split text into words
@@ -21,5 +29,30 @@ function updateWordCount() {
   spanWordElement.textContent = wordCount;
 }
 
-// Update character count on input
-textAreaElement.addEventListener("input", updateWordCount);
+// Twitter character limit
+function updateTwitterCharLimit() {
+  const charCount = textAreaElement.value.length;
+  const charCountLeft = 280 - charCount; // Twitter's character limit is 280
+  spanTwitterElement.textContent = charCountLeft;
+}
+
+// Update char limit on backspace
+textAreaElement.addEventListener("keydown", (event) => {
+  if (event.key === "Backspace") {
+    updateTwitterCharLimit();
+  }
+});
+
+// Facebook character limit
+function updateFacebookCharLimit() {
+  const charCount = textAreaElement.value.length;
+  const charCountLeft = 2200 - charCount; // Facebook's character limit is 280
+  spanFacebookElement.textContent = charCountLeft;
+}
+
+// Update char limit on backspace
+textAreaElement.addEventListener("keydown", (event) => {
+  if (event.key === "Backspace") {
+    updateFacebookCharLimit();
+  }
+});
