5b73d39f36ebcec899f46e24070cd40c1803888e
Alokananda Y <plantsfortrees@gmail.com>
2024-05-03 12:43:44 +0530

    Word count

--- professional-javascript/word-analytics/app.js
+++ professional-javascript/word-analytics/app.js
@@ -1,5 +1,6 @@
 const textAreaElement = document.querySelector(".textarea");
 const spanCharElement = document.querySelector(".stat__number--characters");
+const spanWordElement = document.querySelector(".stat__number--words");
 
 function updateCharCount() {
   const charCount = textAreaElement.value.length;
@@ -8,3 +9,17 @@ function updateCharCount() {
 
 // Update character count on input
 textAreaElement.addEventListener("input", updateCharCount);
+
+function updateWordCount() {
+  const text = textAreaElement.value.trim(); // Remove leading and trailing spaces
+  const words = text.split(/\s+/).filter((word) => word !== ""); // Split text into words
+
+  // Filter out single characters and spaces
+  const validWords = words.filter((word) => word.length > 1);
+
+  const wordCount = validWords.length;
+  spanWordElement.textContent = wordCount;
+}
+
+// Update character count on input
+textAreaElement.addEventListener("input", updateWordCount);
