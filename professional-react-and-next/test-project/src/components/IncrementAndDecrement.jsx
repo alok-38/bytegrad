1594ab9b56ceb7a1fd1ac24495875fd97a91efce
Alokananda Y <plantsfortrees@gmail.com>
2024-05-04 18:17:36 +0530

    Counter

--- professional-react-and-next/test-project/src/components/IncrementAndDecrement.jsx
+++ professional-react-and-next/test-project/src/components/IncrementAndDecrement.jsx
@@ -1,16 +1,36 @@
 import { useState } from "react";
-import { FaArrowUp } from "react-icons/fa";
-import { FaArrowDown } from "react-icons/fa";
+import { FaArrowUp, FaArrowDown } from "react-icons/fa";
+import { MdRefresh } from "react-icons/md";
 
 export default function IncrementAndDecrement() {
   const [count, setCount] = useState(0);
+  const resetCount = () => {
+    // Optionally, add a confirmation prompt here
+    setCount(0);
+  };
   return (
     <div className="flex justify-center flex-col items-center mt-60">
+      {count === 5 && (
+        <div className="mb-5 text-3xl text-cyan-600">
+          You&apos;ve reached 5! Time to celebrate!
+        </div>
+      )}
       <div className="flex gap-10 mb-5 text-5xl">
-        <button className="text-green-500" onClick={() => setCount((count) => count + 1)}>
+        <button
+          className="text-green-500"
+          onClick={() => setCount((count) => count + 1)}
+          disabled={count === 5}
+        >
           <FaArrowUp />
         </button>
-        <button className="text-orange-500" onClick={() => setCount((count) => count - 1)}>
+        <button className="text-red-500" onClick={resetCount}>
+          <MdRefresh />
+        </button>
+        <button
+          className="text-orange-500"
+          onClick={() => setCount((count) => count - 1)}
+          disabled={count === 0}
+        >
           <FaArrowDown />
         </button>
       </div>
