1de2ce869633b12752ec52184f5bd8796439cd11
Alokananda Y <plantsfortrees@gmail.com>
2024-05-04 17:54:39 +0530

    Counter

--- professional-react-and-next/test-project/src/components/IncrementAndDecrement.jsx
+++ professional-react-and-next/test-project/src/components/IncrementAndDecrement.jsx
@@ -5,16 +5,16 @@ import { FaArrowDown } from "react-icons/fa";
 export default function IncrementAndDecrement() {
   const [count, setCount] = useState(0);
   return (
-    <>
-      <div className="flex">
-        <button onClick={() => setCount((count) => count + 1)}>
+    <div className="flex justify-center flex-col items-center mt-60">
+      <div className="flex gap-10 mb-5 text-5xl">
+        <button className="text-green-500" onClick={() => setCount((count) => count + 1)}>
           <FaArrowUp />
         </button>
-        <button onClick={() => setCount((count) => count - 1)}>
+        <button className="text-orange-500" onClick={() => setCount((count) => count - 1)}>
           <FaArrowDown />
         </button>
       </div>
-      <p>{count}</p>
-    </>
+      <p className="text-7xl text-slate-700">{count}</p>
+    </div>
   );
 }
