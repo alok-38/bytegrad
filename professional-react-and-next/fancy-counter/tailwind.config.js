645591d3e6d058f2be368f8cd475676caa61f2fe
Alokananda Y <plantsfortrees@gmail.com>
2024-05-05 13:43:45 +0530

    UI

--- professional-react-and-next/fancy-counter/tailwind.config.js
+++ professional-react-and-next/fancy-counter/tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "radial-gradient": "radial-gradient(ellipse, #385300, #212900)",
+      },
+      boxShadow: {
+        "3xl": "0 35px 60px -15px rgba(0, 0, 0, 0.3)",
+      },
+    },
   },
   plugins: [],
-}
+};
