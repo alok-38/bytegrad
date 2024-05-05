cf9ba1fc1ce1ce2472bc110bdb40786fb96f8362
Alokananda Y <plantsfortrees@gmail.com>
2024-05-05 17:59:58 +0530

    Completed the UI

--- professional-react-and-next/fancy-counter/src/components/Buttons.jsx
+++ professional-react-and-next/fancy-counter/src/components/Buttons.jsx
@@ -2,7 +2,7 @@ import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
 
 export default function Buttons() {
   return (
-    <button className="text-[#bdfe00] opacity-90 flex gap-48 mx-auto items-center justify-center hover: focus:bg-[#212b06]">
+    <button className="text-[#bdfe00] opacity-90 flex gap-48 mx-auto items-center justify-center hover:bg-[#212b06] focus:bg-[#212b06]">
       <MinusIcon className="count-btn-icon count-btn--plus h-[40px] w-[40px] cursor-pointer transition-all" />
       <PlusIcon className="count-btn--plus count-btn-icon h-[40px] w-[40px] cursor-pointer transition-all" />
     </button>
