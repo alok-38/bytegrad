1f87e65613cae963b19c950dd8532e8012e0b318
Alokananda Y <plantsfortrees@gmail.com>
2024-05-21 14:52:45 +0530

    WORDANALYTICS completed

--- professional-javascript/21-05-24/word-analytics/app.js
+++ professional-javascript/21-05-24/word-analytics/app.js
@@ -3,20 +3,17 @@ const spanWordElement = document.querySelector(".stat__number--words");
 const spanCharElement = document.querySelector(".stat__number--characters");
 const spanTwitterElement = document.querySelector(".stat__number--twitter");
 const spanFacebookElement = document.querySelector(".stat__number--facebook");
-let initialWordCount = parseInt(spanWordElement.textContent);
-let initialTwitterCharCount = parseInt(spanTwitterElement.textContent);
-let initialFacebookCharCount = parseInt(spanFacebookElement.textContent);
+
+const initialTwitterCharCount = parseInt(spanTwitterElement.textContent);
+const initialFacebookCharCount = parseInt(spanFacebookElement.textContent);
 
 const acceptInputAndUpdateWordCount = () => {
   const inputText = textAreaElement.value.trim();
   const wordCount = inputText
-    .split(/\s+/) // Split by any whitespace character
-    .filter((word) => word.trim().length > 1).length; // Filter out words with less than 2 characters
-  // Update the UI
+    .split(/\s+/)
+    .filter((word) => word.trim().length > 1).length;
   spanWordElement.textContent = wordCount;
 };
-
-textAreaElement.addEventListener("input", acceptInputAndUpdateWordCount);
 
 const acceptInputAndUpdateCharCount = () => {
   const inputText = textAreaElement.value;
@@ -24,22 +21,21 @@ const acceptInputAndUpdateCharCount = () => {
   spanCharElement.textContent = charCount;
 };
 
-textAreaElement.addEventListener("input", acceptInputAndUpdateCharCount);
-
 const acceptInputAndUpdateTwitterCount = () => {
   const inputText = textAreaElement.value;
-  const twitterCharCount = inputText.length;
-  let twitterCharCountLeft = initialTwitterCharCount - twitterCharCount;
-  spanTwitterElement.textContent = twitterCharCountLeft;
+  const twitterCharCountLeft = initialTwitterCharCount - inputText.length;
+  spanTwitterElement.textContent =
+    twitterCharCountLeft >= 0 ? twitterCharCountLeft : 0;
 };
-
-textAreaElement.addEventListener("input", acceptInputAndUpdateTwitterCount);
 
 const acceptInputAndUpdateFacebookCount = () => {
   const inputText = textAreaElement.value;
-  const facebookCharCount = inputText.length;
-  let facebookCharCountLeft = initialFacebookCharCount - facebookCharCount;
-  spanFacebookElement.textContent = facebookCharCountLeft;
+  const facebookCharCountLeft = initialFacebookCharCount - inputText.length;
+  spanFacebookElement.textContent =
+    facebookCharCountLeft >= 0 ? facebookCharCountLeft : 0;
 };
 
+textAreaElement.addEventListener("input", acceptInputAndUpdateWordCount);
+textAreaElement.addEventListener("input", acceptInputAndUpdateCharCount);
+textAreaElement.addEventListener("input", acceptInputAndUpdateTwitterCount);
 textAreaElement.addEventListener("input", acceptInputAndUpdateFacebookCount);
