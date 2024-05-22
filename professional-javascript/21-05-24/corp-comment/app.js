7f8672d7fa4441ee2fdddd54003e78f136831d45
Alokananda Y <plantsfortrees@gmail.com>
2024-05-22 07:33:48 +0530

    submitHandler

--- professional-javascript/21-05-24/corp-comment/app.js
+++ professional-javascript/21-05-24/corp-comment/app.js
@@ -1,11 +1,33 @@
+const formElement = document.querySelector(".form");
 const textAreaElement = document.querySelector("textarea");
 const counterElement = document.querySelector(".counter");
-let initialCharCount = parseInt(textAreaElement.getAttribute("maxlength"));
+const feedbackListElement = document.querySelector(".feedbacks");
+const hashtagElements = document.querySelectorAll(".hashtag");
+const initialCharCount = parseInt(textAreaElement.getAttribute("maxlength"));
+let feedbacks = [];
 
 const textAreaInputCharsLeft = () => {
   const acceptInputValue = textAreaElement.value;
-  let finalCharCount = initialCharCount - acceptInputValue.length;
+  const finalCharCount = initialCharCount - acceptInputValue.length;
   counterElement.textContent = finalCharCount;
 };
 
 textAreaElement.addEventListener("input", textAreaInputCharsLeft);
+
+const submitHandler = (event) => {
+  event.preventDefault();
+  const text = textAreaElement.value;
+  if (text.includes("#")) {
+    formElement.classList.add("form--valid");
+    setTimeout(() => {
+      formElement.classList.remove("form--valid");
+    }, 2000);
+  } else {
+    formElement.classList.add("form--invalid");
+    setTimeout(() => {
+      formElement.classList.remove("form--invalid");
+    }, 2000);
+  }
+};
+
+formElement.addEventListener("submit", submitHandler);
