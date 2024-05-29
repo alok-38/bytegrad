1443e89e39311a2e1bd4890eded0a84a1b5fd50f
Alokananda Y <plantsfortrees@gmail.com>
2024-05-29 18:06:24 +0530

    My First Component

---
+++ coding-addict/react-course/29-05-24/app.js
@@ -0,0 +1,10 @@
+const app = document.getElementById("app");
+
+// arrow function also works
+
+const Greeting = () => {
+  return <h2>My First Component</h2>;
+};
+
+const root = ReactDOM.createRoot(app);
+root.render(<Greeting />);
