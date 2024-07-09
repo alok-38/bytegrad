ee1d6487b18e5a0e7582d1c0d0f7ac5b99b7363e
Alokananda Y <plantsfortrees@gmail.com>
2024-07-09 12:21:19 +0530

    Word analytics

---
+++ professional-javascript/word-analytics/app.js
@@ -0,0 +1,6 @@
+const textareaElement = document.querySelector('.textarea');
+const wordsCountElement = document.querySelector('.stat__number--words');
+const characterCountElement = document.querySelector('.stat__number--characters');
+const twitterCharaterElement = document.querySelector('.stat__number--twitter');
+const facebookCharaterElement = document.querySelector('.stat__number--facebook');
+
