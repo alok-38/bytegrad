f6c93690bc58091b90e628aeaa7968154826ac86
Alokananda Y <plantsfortrees@gmail.com>
2024-07-12 08:16:41 +0530

    This is the main layout

--- professional-react-and-next/next14-tutorial/src/app/page.js
+++ professional-react-and-next/next14-tutorial/src/app/page.js
@@ -1,7 +1,7 @@
-export default function Home() {
+const Home = () => {
   return (
-    <main>
-      <h1>Hello World</h1>
-    </main>
-  );
+    <div>Home page</div>
+  )
 }
+
+export default Home
