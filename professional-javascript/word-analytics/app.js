a671f2bfa131e9c2711d7c81e1ce370137bec1fa
Alokananda Y <plantsfortrees@gmail.com>
2024-12-12 04:32:25 +0000

    Working with char count

--- professional-javascript/word-analytics/app.js
+++ professional-javascript/word-analytics/app.js
@@ -8,12 +8,20 @@ const facebookSpanEl = document.querySelector('.stat__number--facebook');
 function countWords(textInput) {
     // Split the text into words by spaces and filter out any empty strings
     const words = textInput.trim().split(/\s+/);
-    console.log("Word count:", words.filter(word => word.length > 0).length);
+    wordSpanEl.textContent = words.filter(word => word.length > 0).length;
+}
+
+// Function to count characters
+function countChars(textInput) {
+    // calculate the length of the string
+    const numberOfChars = textInput.length;
+    charSpanEl.textContent = numberOfChars;
 }
 
 function textAnalytics() {
     const textInput = textareaEl.value;  // Get the current value from the textarea
     countWords(textInput);  // Pass the value to the countWords function
+    countChars(textInput);
 }
 
 // Add an event listener to count words as the user types
